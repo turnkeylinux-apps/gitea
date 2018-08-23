fbc88555d085d7f0eb1477f6d9b63385e916d53a
Anton Pyrogovskyi <anton@turnkeylinux.org>
2018-08-23 22:58:30 +0200

    First commit
    Closes https://github.com/turnkeylinux/tracker/issues/1049

---
+++ README.rst
@@ -0,0 +1,32 @@
+Gitea - Git with a cup of tea!
+===================================
+
+`Gitea`_ is a painless self-hosted Git service. It is similar to GitHub,
+Bitbucket and Gitlab. Gitea is a fork of Gogs, a lightweight code
+hosting solution written in Go and published under the MIT license.
+
+This appliance includes all the standard features in `TurnKey Core`_,
+and on top of that:
+
+- Gitea configurations:
+   
+   - Gitea installed from latest upstream.
+   - Set Gitea admin password and email on firstboot (convenience,
+     security).
+   - Set Gitea domain to serve on first boot (convenience).
+   - Pre-configured to use MySQL (recommended for production).
+   - Includes Nginx pre-configured to proxy to Gitea daemon, with SSL
+     support out of the box (performance, security).
+
+- Includes postfix MTA (bound to localhost) for sending of email (e.g.
+  password recovery). Also includes webmin postfix module for
+  convenience.
+
+Credentials *(passwords set at first boot)*
+-------------------------------------------
+
+-  Webmin, SSH, MySQL, Adminer: username **root**
+-  Gitea: username is **gitea**, password is set at first boot
+
+.. _Gitea: https://gitea.io
+.. _TurnKey Core: https://www.turnkeylinux.org/core
---
+++ overlay/usr/lib/inithooks/bin/gitea.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+"""Set Gitea admin password, email and domain to serve
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+    --domain=   unless provided, will ask interactively
+                DEFAULT=www.example.com
+"""
+
+import sys
+import getopt
+import inithooks_cache
+from mysqlconf import MySQL
+
+from dialog_wrapper import Dialog
+from executil import ExecError, system
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+DEFAULT_DOMAIN="www.example.com"
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'email=', 'domain='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    email = ""
+    domain = ""
+    password = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--email':
+            email = val
+        elif opt == '--domain':
+            domain = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "Gitea Password",
+            "Enter new password for the Gitea 'admin' account.",
+            pass_req = 8)
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email(
+            "Gitea Email",
+            "Enter email address for the Gitea 'admin' account.",
+            "admin@example.com")
+
+    inithooks_cache.write('APP_EMAIL', email)
+
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "Gitea Domain",
+            "Enter the domain to serve Gitea.",
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
+    inithooks_cache.write('APP_DOMAIN', domain)
+
+    config = "/etc/gitea/app.ini"
+    system("su git -c 'cd /home/git && ./gitea admin change-password -u gitea -p %s'" % password)
+    system('sed -i "s|\(DOMAIN =\).*|\1 %s|" %s' % (domain, config))
+
+    m = MySQL()
+    m.execute('UPDATE gitea.user SET email=\"%s\" WHERE id=%i;' % (email, 1))
+
+    try:
+        system("systemctl restart gitea")
+    except ExecError:
+        pass
+
+if __name__ == "__main__":
+    main()
+
